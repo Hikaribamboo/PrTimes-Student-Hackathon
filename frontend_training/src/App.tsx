fcb1ac04c60c0dabafd37cfa1c78fbe3dad68b25
Hikaribamboo <hikari1653@gmail.com>
2025-02-24 19:30:32 +0900

    made frontend

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -1,12 +1,41 @@
-import './App.css'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState<{ id: string; text: string }[]>([]);
+
+  const addTask = () => {
+    const newTask = { id: uuidv4(), text: task };
+    setTasks([...tasks, newTask]);
+    setTask('');
+  };
+
+  const completeTask = (id: string) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   return (
-    <>
-      <h1>TODOアプリ</h1>
-    </>
-  )
+    <div>
+      <input
+        type="text"
+        placeholder="TODOを入力"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>追加</button>
+
+      <ul>
+        {tasks.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => completeTask(todo.id)}>完了</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
